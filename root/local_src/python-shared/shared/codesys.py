1703e110515b9aeed8fd772eb85cd554f751ac26
Artur Wiebe <artur@4wiebe.de>
2023-10-13 12:33:36 +0200

    update codesys runtime to 4.9.0.0

--- root/local_src/python-shared/shared/codesys.py
+++ root/local_src/python-shared/shared/codesys.py
@@ -15,50 +15,52 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 
-from __future__ import annotations
+import ctypes
 import re
-from ctypes import *
 from pathlib import Path
 
 
 types = {
-	'BOOL':		c_bool,
-	'BYTE':		c_byte,
-	'SINT':		c_int8,
-	'INT':		c_int16,
-	'DINT':		c_int32,
-	'LINT':		c_int64,
-	'USINT':	c_uint8,
-	'UINT':		c_uint16,
-	'UDINT':	c_uint32,
-	'ULINT':	c_uint64,
-	'REAL':		c_float,
-	'LREAL':	c_double,
+	'BOOL':		ctypes.c_bool,
+	'SINT':		ctypes.c_int8,
+	'INT':		ctypes.c_int16,
+	'DINT':		ctypes.c_int32,
+	'LINT':		ctypes.c_int64,
+	'USINT':	ctypes.c_uint8,
+	'BYTE':		ctypes.c_uint8,
+	'UINT':		ctypes.c_uint16,
+	'WORD':		ctypes.c_uint16,
+	'UDINT':	ctypes.c_uint32,
+	'DWORD':	ctypes.c_uint32,
+	'ULINT':	ctypes.c_uint64,
+	'LWORD':	ctypes.c_uint64,
+	'REAL':		ctypes.c_float,
+	'LREAL':	ctypes.c_double,
 }
 
 
 class Field(tuple):
 	def __new__(cls, name, ctype, comment):
-		self = tuple.__new__(cls, (name, ctype))
-		self.name = name
-		self.ctype = ctype
-		self.comment = comment
-		return self
+		return tuple.__new__(cls, (name, ctype))
+
+	def __init__(self, name, ctype, comment):
+		self.name, self.ctype, self.comment = name, ctype, comment
 
 
-def parse_struct(name:str) -> Structure:
+def parse_struct(name:str) -> type[ctypes.Structure]:
 	file_text = Path('/usr/share/codesys', f'{name}.struct').read_text()
 	
 	def ctype(codesys_type):
+		array_length = None
 		if array := re.search(r'ARRAY \[(\d+)\.\.(\d+)\] OF (\w+)', codesys_type):
 			codesys_type = array.group(3)
 			array_length = int(array.group(2)) - int(array.group(1)) + 1
 		if codesys_type not in types:
 			types[codesys_type] = parse_struct(codesys_type)
-		return types[codesys_type] * array_length if array else types[codesys_type]
+		return types[codesys_type] * array_length if array_length else types[codesys_type]
 	
 	return type(
 		name,
-		(Structure, ),
+		(ctypes.Structure, ),
 		{'_fields_': [Field(f[0], ctype(f[1]), f[2]) for f in re.findall(r'^\s*(\w+)\s*:\s*(.+)\s*;\s*(?://\s*(.*)\s*)?$', file_text, re.MULTILINE)]},
 	)
